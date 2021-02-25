d0189eb0b7cb0158f874689c1000ca542ce20a5c
Audboy <79108562+Audboy@users.noreply.github.com>
2021-02-25 16:16:51 +0000

    Initial version

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    private string currentState = "IdleState";
+    private Transform target;
+    public float chaseRange = 5;
+    public float speed = 3;
+    public Animator animator;
+    void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float distance = Vector3.Distance(transform.position, target.position);
+
+        if(currentState == "IdleState")
+        {
+            if (distance < chaseRange)
+                currentState = "ChaseState";
+        }else if(currentState == "ChaseState")
+        {
+            animator.SetTrigger("chase");
+
+            if (target.position.x > transform.position.x)
+            {
+                transform.Translate(transform.right * speed * Time.deltaTime);
+            }
+            else
+            {
+                transform.Translate(-transform.right * speed * Time.deltaTime);
+            }
+        }
+    }
+}
